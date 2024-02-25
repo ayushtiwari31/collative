015ba5787081a6cd2848721b36f036d9098db570
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2024-02-25 17:31:49 +0530

    Changes

--- collative/src/App.jsx
+++ collative/src/App.jsx
@@ -19,10 +19,7 @@ function App() {
     <Navbar/>
     <Home/>
     {/* <Explore/> */}
-<<<<<<< HEAD
     <Harmony/>
-=======
->>>>>>> fd600d943c4467724bccfeff6fd0baa2a42d6fba
     <Testimonial/>
     <SliderPage/>
     <Features/>
